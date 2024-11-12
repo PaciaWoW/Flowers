bab0cd23bf37ddcde99fa9e97c308fb1ac24c23e
PaciaWoW <zacharypaddie4@gmail.com>
2024-11-11 23:16:13 -0800

    main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-
+n
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('ENJOY!!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
